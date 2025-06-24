f14f700cac4870be5702969092a824a98b222fe8
janghyein <53409567+janghyein@users.noreply.github.com>
2025-06-25 01:16:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
 export const WEDDING_DATE = "2025년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "웨딩, ○층 ○○홀";
+export const WEDDING_LOCATION = "1111111111웨딩, ○층 ○○홀";
 
 export const GROOM_NAME = "일번";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
